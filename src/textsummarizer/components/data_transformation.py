fd4c1dfd15a9f9eb8061c61a13e64637eb1acbf0
Pari Manouchehri <parichehr.ma@gmail.com>
2026-05-13 21:06:30 -0700

    model trainer added

--- src/textsummarizer/components/data_transformation.py
+++ src/textsummarizer/components/data_transformation.py
@@ -8,44 +8,39 @@ class DataTransformation:
 
     def __init__(self, config):
         self.config = config
-
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            config.tokenizer_name,
-            use_fast=False
-
-        )
+        self.tokenizer = AutoTokenizer.from_pretrained(config.tokenizer_name)
 
     def convert_examples_to_features(self, example_batch):
 
-        # Tokenize dialogue
+        input_texts = ["summarize: " + text for text in example_batch['dialogue']]
+
         input_encodings = self.tokenizer(
-            example_batch['dialogue'], # Converts input conversation into tokens.
-            max_length=1024, 
+            input_texts,
+            max_length=1024,
             truncation=True,
             padding='max_length'
         )
 
-        # Tokenize summary
         target_encodings = self.tokenizer(
-            text_target=example_batch['summary'], # Converts target summary into labels.
+            text_target=example_batch['summary'],
             max_length=128,
             truncation=True,
-            padding='max_length' # Makes all sequences same length.Needed for batching during training.
+            padding='max_length'
         )
+
+        labels = target_encodings["input_ids"]
 
         return {
             'input_ids': input_encodings['input_ids'],
             'attention_mask': input_encodings['attention_mask'],
-            'labels': target_encodings['input_ids']
+            'labels': labels
         }
 
     def convert(self):
 
         print("Loading dataset...")
 
-        dataset_samsum = load_from_disk(
-            self.config.data_path
-        )
+        dataset_samsum = load_from_disk(self.config.data_path)
 
         print("Transforming dataset...")
 
@@ -56,8 +51,6 @@ class DataTransformation:
 
         print("Saving transformed dataset...")
 
-        dataset_samsum_pt.save_to_disk(
-            self.config.root_dir
-        )
+        dataset_samsum_pt.save_to_disk(self.config.root_dir)
 
         print("Transformation completed successfully.")
---
+++ src/textsummarizer/components/model_trainer.py
@@ -0,0 +1,92 @@
+from transformers import TrainingArguments, Trainer  
+from transformers import DataCollatorForSeq2Seq  
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer  
+from datasets import load_dataset, load_from_disk 
+from textsummarizer.entity import ModelTrainerConfig
+import torch  
+import os  
+
+
+
+class ModelTrainer:
+    def __init__(self, config: ModelTrainerConfig): 
+        self.config = config  
+
+    
+    def train(self):
+
+       
+        device = "cuda" if torch.cuda.is_available() else "cpu" 
+
+        
+        tokenizer = AutoTokenizer.from_pretrained(self.config.model_ckpt) 
+        model_pegasus = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_ckpt).to(device) 
+
+        
+        seq2seq_data_collator = DataCollatorForSeq2Seq(tokenizer, model=model_pegasus) 
+
+        
+        dataset_samsum_pt = load_from_disk(self.config.data_path) 
+
+        print(self.config.eval_steps, type(self.config.eval_steps))
+        print(self.config.save_steps, type(self.config.save_steps))
+        print(self.config.num_train_epochs, type(self.config.num_train_epochs))
+
+        # =========================================================================
+        # OPTION 1: CONFIG-BASED (BEST PRACTICE - RECOMMENDED) - USING yaml FILE
+        # ==========================================================================
+        trainer_args = TrainingArguments(
+            output_dir=self.config.root_dir,  
+            num_train_epochs=self.config.num_train_epochs,  
+            warmup_steps=self.config.warmup_steps,  
+            per_device_train_batch_size=self.config.per_device_train_batch_size,  
+            per_device_eval_batch_size=self.config.per_device_train_batch_size,  
+            weight_decay=self.config.weight_decay,  
+            logging_steps=self.config.logging_steps,  
+            #evaluation_strategy=self.config.evaluation_strategy,
+            eval_strategy=self.config.evaluation_strategy,  
+            # evaluation_strategy=self.config.eval_strategy,
+            eval_steps=self.config.eval_steps,  
+            save_steps=1e6,  
+            gradient_accumulation_steps=self.config.gradient_accumulation_steps  
+        )
+        # ==========================================================
+        # OPTION 2: HARDCODED (ONLY FOR TESTING)
+        # ==========================================================
+        """
+        trainer_args = TrainingArguments(
+            output_dir=self.config.root_dir,  # where model will be saved
+            num_train_epochs=1,  # how many times model sees data. model sees dataset only once
+            warmup_steps=500,  # slow start for stable training
+            per_device_train_batch_size=1,  # training batch size. very small batch (good for weak GPU)
+            per_device_eval_batch_size=1,  # evaluation batch size
+            weight_decay=0.01,  # prevents overfitting. standard overfitting control
+            logging_steps=10,  # show logs every 10 steps
+            evaluation_strategy='steps',  # evaluate during training
+            eval_steps=500,  # evaluate every 500 steps
+            save_steps=1e6,  # save model rarely (almost at end)
+            gradient_accumulation_steps=16  # simulate bigger batch using memory efficiency
+        )
+        """
+        
+        trainer = Trainer(  
+            model=model_pegasus, 
+            args=trainer_args,  
+            #tokenizer=tokenizer,  
+            data_collator=seq2seq_data_collator,  
+            train_dataset=dataset_samsum_pt["test"],  # I use test data for get faster result)
+            eval_dataset=dataset_samsum_pt["validation"]  
+        )
+
+        # start training process
+        trainer.train()
+
+        # save trained model
+        model_pegasus.save_pretrained(
+            os.path.join(self.config.root_dir, "pegasus-samsum-model")
+        )
+
+        # save tokenizer for later use
+        tokenizer.save_pretrained(
+            os.path.join(self.config.root_dir, "tokenizer")
+        )
